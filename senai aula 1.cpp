33dfebbdc25943e4bfe54ea08bd17a492dd483b3
luxxaa12 <159189844+luxxaa12@users.noreply.github.com>
2024-02-27 15:10:57 -0300

    Add files via upload

---
+++ senai aula 1.cpp
@@ -0,0 +1,17 @@
+//Biblioteca
+// std-i-o
+//std = standard (padrao)
+// i = input(entrada)
+//o = output (saida)
+#include <stdio.h>//sem o codigo nao roda
+//Função principal
+int main(){
+	// Comando para mostrar na tela
+	prin
+	printf("ola lucas !");
+	
+	// infromar que o programa terminou	
+	return 0;
+	
+}
+
