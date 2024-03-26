2626ec7619ff5f52f98a2e54cc15463766703124
luxxaa12 <159189844+luxxaa12@users.noreply.github.com>
2024-03-26 16:05:53 -0300

    Add files via upload

---
+++ media aluno matriz.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <locale.h>
+#include <time.h>
+#define tam 3
+
+
+int main(){
+	setlocale(LC_ALL,"");
+	int i,j;
+	float notas [4][tam];
+	char nomes[4][200];
+	float media[4];
+	float soma = 0;
+	
+
+
+	for (i = 0; i < 4; i++){
+		printf("Digite o  nome do %iª aluno:", i+1);
+		scanf("%s",&nomes[i]);
+		
+		for (j = 0; j < tam; j++){
+			printf("Digite a %iª Nota:",j+1);
+			scanf("%f",&notas[i][j]);
+			
+			soma += notas[i][j];
+			
+		}
+		
+		media[i] = soma / j;
+		soma = 0;
+		
+		
+	}
+	
+	for (i = 0;i <4;i++){
+		printf("%iª aluno: %s\n", i+1,nomes[i]);
+		for(j = 0; j < tam; j++){
+			printf("%iª nota: %.1f \n", j+1,notas[i][j]);
+
+}
+
+printf("Media: %.1f \n",media[i]);	
+
+		}
+}
